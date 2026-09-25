485f5fef39291de699d942dd2fd7ef559e66858e
BitKioto <antony_lovon@usmp.pe>
2026-09-24 22:59:37 -0500

    Initial commit - Proyecto base ASP.NET Core MVC con Identity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practica2AppCreditos.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
